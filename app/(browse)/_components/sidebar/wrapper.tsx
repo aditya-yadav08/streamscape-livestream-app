094347ceac02ee8685002825d4dd2c8fe3bf747b
aditya-yadav08 <aditya.yadav2708@gmail.com>
2024-06-10 17:47:33 +0530

    sidebar - recommended list

--- app/(browse)/_components/sidebar/wrapper.tsx
+++ app/(browse)/_components/sidebar/wrapper.tsx
@@ -2,6 +2,9 @@
 
 import { useSidebar } from "@/store/use-sidebar";
 import { cn } from "@/lib/utils";
+import { useEffect, useState } from "react";
+import { ToggleSkeleton } from "./toggle";
+import { RecommendedSkeleton } from "./recommended";
 
 interface WrapperProps {
     children: React.ReactNode;
@@ -11,7 +14,21 @@ export const Wrapper = ({
     children,
 }: WrapperProps) => {
 
+    const [isClient, setIsClient] = useState(false);
     const { collapsed } = useSidebar((state) => state); 
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    if (!isClient) {
+        return (
+        <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
+            <ToggleSkeleton/>
+            <RecommendedSkeleton/>
+        </aside>
+        ); 
+    }
 
     return (
         <aside
